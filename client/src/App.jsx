3e0912c3d0303bca6a76d3903be0986b3272e380
Anmol Jhamb <talktoanmol@outlook.com>
2022-12-24 20:37:35 +0530

    Rename App.js to App.jsx, index.js to index.jsx. Added the dependency for sass

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    /**
+     * App will be the single page application of react.
+     * It will have all the different routes.
+     */
+
+    return <h1>Hello World</h1>;
+}
+
+export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+/**
+ * <App/> Needs to be covered in <BrowserRouter></BrowserRouter>
+ * to make sure that routing works in App.jsx
+ */
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
