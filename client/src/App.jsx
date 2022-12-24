86e362643e11e144f5f7d40db2db106637d6676c
Anmol Jhamb <talktoanmol@outlook.com>
2022-12-24 20:47:16 +0530

    Basic outline of things that need to be added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,23 @@ import React from "react";
 function App() {
     /**
      * App will be the single page application of react.
-     * It will have all the different routes.
+     * * It will have all the different routes.
+     * * App Routes the front end. i.e,
+     *
+     * * Pages to make
+     * todo HomePage
+     * todo About Page
+     * todo Explore page
+     * todo Blog Page
+     *
+     * * Common Components
+     * todo Navbar
+     * todo Header that has an image with parallax effect with a bunch of text on it.
+     * todo Footer
+     * todo add made with love by anmol
+     *
+     * ! All the other components will be separated page wise in components folder.
+     *
      */
 
     return <h1>Hello World</h1>;
