911d41759d74fa2ae442b83190d703c2c7303b6c
Anmol Jhamb <talktoanmol@outlook.com>
2022-12-25 14:58:36 +0530

    added the schema for both testimonials and places

--- server/db/models/Place.js
+++ server/db/models/Place.js
@@ -1,3 +1,31 @@
 /**
  * * Cotains the mongoose Schema and the model for all the places our website will feature.
  */
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+
+const placeSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    fact: {
+        type: String,
+        required: true,
+    },
+    images: {
+        type: Array,
+        required: true,
+    },
+    texts: {
+        type: Array,
+        required: true,
+    },
+    featured: {
+        type: Boolean,
+    },
+});
+
+module.exports = new model("Place", placeSchema);
--- server/db/models/Testimonials.js
+++ server/db/models/Testimonials.js
@@ -1,3 +1,24 @@
 /**
  * * Cotains the mongoose Schema and the model for all the Testimonials our website will feature.
  */
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+
+const testimonialSchema = Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    review: {
+        type: String,
+        required: true,
+    },
+    profileLink: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = new model("Testimonial", testimonialSchema);
