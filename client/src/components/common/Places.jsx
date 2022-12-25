d07f9b4d742094680b52da830525557d5e0cc863
Anmol Jhamb <talktoanmol@outlook.com>
2022-12-25 18:55:25 +0530

    places component

---
+++ client/src/components/common/Places.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Place from "../ExplorePage/Place";
+
+const Places = ({ compPlaces }) => {
+    return compPlaces.map((place) => {
+        return (
+            <Place
+                key={place._id}
+                name={place.name}
+                image={place.images[0]}
+                fact={place.fact}
+            ></Place>
+        );
+    });
+};
+export default Places;
