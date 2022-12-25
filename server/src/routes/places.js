6ffcfc4afdceb4bd7c70173039b57566988f955f
Anmol Jhamb <talktoanmol@outlook.com>
2022-12-25 16:46:25 +0530

    requests for get featured, post, delete

--- server/src/routes/places.js
+++ server/src/routes/places.js
@@ -9,10 +9,52 @@ const Place = require("../../db/models/Place");
 router.get("/", async (req, res) => {
     try {
         const places = await Place.find().exec();
-        res.status(200).json(places);
+        res.status(200).json({ places });
     } catch (err) {
         res.status(500).json({
             message: "There was an error while fetching places",
+            err,
+        });
+    }
+});
+
+router.get("/featured", async (req, res) => {
+    try {
+        const places = await Place.find({ featured: true }).exec();
+        res.status(200).json(places);
+    } catch (err) {
+        res.status(400).json({
+            message: "There was an error while fetching places",
+            err,
+        });
+    }
+});
+
+router.post("/", async (req, res) => {
+    try {
+        const place = new Place(req.body.place);
+        await place.save();
+        res.status(200).json(place);
+    } catch (err) {
+        res.status(400).json({
+            message: "Bad Request. Please check the data once again.",
+            err,
+        });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    const data = await Place.findById(req.params.id).exec();
+    console.log(data);
+
+    try {
+        await data.remove();
+        res.status(200).json({
+            message: "Delete was successful.",
+        });
+    } catch (err) {
+        res.status(400).json({
+            message: "There was an error while deleting the record.",
             err,
         });
     }
