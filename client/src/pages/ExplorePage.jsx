bc4c43805d5531eb470a2463d0c777df7642a496
Anmol Jhamb <talktoanmol@outlook.com>
2022-12-25 18:48:40 +0530

    ExplorePage completed

--- client/src/pages/ExplorePage.jsx
+++ client/src/pages/ExplorePage.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import BgImageWithText from "../components/common/BgImageWithText";
+import Place from "../components/ExplorePage/Place";
 import ExploreImage from "../images/explore.jpg";
 import "../style/pages/ExplorePage.scss";
 
 const ExplorePage = () => {
+    const [places, setPlaces] = useState([]);
+
+    useMemo(() => {
+        fetch(`${process.env.REACT_APP_BACKEND_URI}/place`).then((resp) => {
+            resp.json().then((data) => {
+                setPlaces(data.places);
+            });
+        });
+    }, []);
+
+    const Places = () => {
+        return places.map((place) => {
+            return (
+                <Place
+                    key={place._id}
+                    name={place.name}
+                    image={place.images[0]}
+                    fact={place.fact}
+                ></Place>
+            );
+        });
+    };
+
     return (
         <>
             <BgImageWithText
                 src={ExploreImage}
                 text="Explore"
             ></BgImageWithText>
+            <section>
+                <div className="places">
+                    <Places></Places>
+                </div>
+            </section>
         </>
     );
 };
